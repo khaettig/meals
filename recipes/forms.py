b60d0f57f5f95f4971c5f0f1c2b8bb34edaae7c6
Kevin Hättig <kevin.haettig@posteo.de>
2022-02-26 17:02:22 +0100

    Allow recipe titles up to 100 characters

--- recipes/forms.py
+++ recipes/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class AddRecipeForm(forms.Form):
-    name = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=100)
     description = forms.CharField(
         widget=forms.Textarea(attrs={"rows": 10, "cols": 80}), required=False
     )
--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True)
     url = models.URLField(null=True)
