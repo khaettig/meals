030a80d86a077b4495c842074927c216b76c2843
Kevin Hättig <kevin.haettig@posteo.de>
2022-02-27 18:10:45 +0100

    Use flex for form sizing

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class AddRecipeForm(forms.Form):
     name = forms.CharField(max_length=100)
     description = forms.CharField(
-        widget=forms.Textarea(attrs={"rows": 10, "cols": 80}), required=False
+        widget=forms.Textarea(attrs={"rows": 10, "cols": 20}), required=False
     )
     url = forms.URLField(required=False)
